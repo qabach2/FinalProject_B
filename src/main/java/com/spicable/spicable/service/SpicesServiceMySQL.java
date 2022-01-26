39c949a752cd74200e9132f35cb7fe724784ea3e
Selam <legese.selina2000@gmail.com>
2022-01-26 16:55:47 -0600

    debug service files

--- src/main/java/com/spicable/spicable/service/SpicesServiceMySQL.java
+++ src/main/java/com/spicable/spicable/service/SpicesServiceMySQL.java
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class SpicesServiceMySQL {
-    public class SpiceServiceMySQL implements SpiceService
+public class SpicesServiceMySQL implements SpiceService
     {
         private final SpicesRepository spicesRepository;
 
-        public SpiceServiceMySQL(@Autowired SpicesRepository spicesRepository )
+        public SpicesServiceMySQL(@Autowired SpicesRepository spicesRepository )
         {
             this.spicesRepository = spicesRepository;
         }
@@ -27,9 +27,10 @@ public class SpicesServiceMySQL {
         }
 
         @Override
-        public void delete(int spiceId)
+        public boolean delete(int spiceId)
         {
             spicesRepository.deleteById( spiceId );
+            return false;
         }
 
         @Override
@@ -39,7 +40,20 @@ public class SpicesServiceMySQL {
             spicesRepository.findAll().forEach( result::add );
             return result;
         }
+
+        @Override
+        public Spices findById(int id){
+           Optional<Spices> optionalSpice = spicesRepository.findById(id);
+           Spices spice = null;
+           if (optionalSpice.isPresent()){
+               spice = optionalSpice.get();
+
+           }else{
+               throw new RuntimeException("spice not for id ::" + id);
+           }
+           return spice;
+        }
 }
-}
+
 
 
